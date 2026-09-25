534661cdcb32b7d3e1970a3468355a1cf4b5e1df
Judin kg <judinkg2003@gmail.com>
2026-09-25 23:03:55 +0530

    Add INTERNET permission to AndroidManifest.xml

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -7,8 +7,8 @@ plugins {
 
 android {
     namespace = "com.alnadheera.al_nadheera_construction"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 36
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
